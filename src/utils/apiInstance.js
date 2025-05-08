af8c22838477f5d2de25978a4f4da0c0c061f05c
DarshanNagesh10 <darshann.801f@gmail.com>
2025-05-08 22:09:51 +0530

    Fixes 09

--- src/utils/apiInstance.js
+++ src/utils/apiInstance.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://d2bgut31xyvm83.cloudfront.net/api/";
+// const API_BASE_URL = "https://d2bgut31xyvm83.cloudfront.net/api/";
+const API_BASE_URL = "https://hack.opqtech.ai/api/";
 // const API_BASE_URL = "http://localhost:5000/api/";
 
 const apiInstance = axios.create({
